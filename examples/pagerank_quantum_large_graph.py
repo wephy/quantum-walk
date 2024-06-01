a7385bfce686e98ca387fa3f1fd761cb8531df5d
unknown <joe@wephy.com>
2024-06-02 00:22:30 +0100

    There was a bug for 2 years??? Fixed, along with updated readme showing the examples

--- examples/pagerank_quantum_large_graph.py
+++ examples/pagerank_quantum_large_graph.py
@@ -6,6 +6,9 @@ against the regular Google PageRank solution.
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import scienceplots
+
+plt.style.use("science")
 
 # Import quantum walk code
 import importer  # noqa # pylint: disable=unused-import
@@ -14,22 +17,29 @@ from quantum_walk import pagerank  # noqa # pylint: disable=import-error
 
 def plot(classical_solution, quantum_solution):
     """Create plot."""
-    fig = plt.figure(constrained_layout=True)
+    fig = plt.figure(constrained_layout=True, figsize=(8, 4))
     solution_fig, quantum_fig = fig.subfigures(nrows=2, ncols=1)
+
     solution_fig.suptitle("classical solution", fontsize=16)
     ax = solution_fig.subplots(1, 1)
     ax.bar(*zip(*classical_solution.items()))
+    ax.get_xaxis().set_ticks([])
+    ax.get_yaxis().set_ticks([])
+
     quantum_fig.suptitle("quantum solution", fontsize=16)
     ax = quantum_fig.subplots(1, 1)
     ax.bar(*zip(*quantum_solution.items()))
+    ax.get_xaxis().set_ticks([])
+    ax.get_yaxis().set_ticks([])
 
     # Show fig
+    plt.savefig("example_quantum_large_graph.png", dpi=600)
     plt.show()
 
 
 def main():
     """Run example
-    
+
     We construct a random large directed graph with the Erdős-Rényi model (we specify
     the number of nodes desired, and provide an edge creation probability to be used
     for every possible edge). We then compare the classical and quantum performance.
--- examples/pagerank_quantum_vs_classical.py
+++ examples/pagerank_quantum_vs_classical.py
@@ -6,6 +6,9 @@ walk along the graph), comparing their results at different stages.
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import scienceplots
+
+plt.style.use("science")
 
 # Import quantum walk code
 import importer  # noqa # pylint: disable=unused-import
@@ -14,22 +17,29 @@ from quantum_walk import pagerank  # noqa # pylint: disable=import-error
 
 def plot(q_results, c_results, plot_points):
     """Create plot."""
-    fig = plt.figure(constrained_layout=True)
+    fig = plt.figure(constrained_layout=True, figsize=(8, 4))
     classical_fig, quantum_fig = fig.subfigures(nrows=2, ncols=1)
+
     classical_fig.suptitle("classical", fontsize=16)
     axs = classical_fig.subplots(nrows=1, ncols=len(plot_points), sharey=True)
     plt.ylim((0, 0.3))
     for i, result in enumerate(c_results):
         axs[i].bar(*zip(*result.items()))
         axs[i].set_title(f"{plot_points[i]} steps")
+        axs[i].get_xaxis().set_ticks([])
+        axs[i].get_yaxis().set_ticks([])
+
     quantum_fig.suptitle("quantum", fontsize=16)
     axs = quantum_fig.subplots(nrows=1, ncols=len(plot_points), sharey=True)
     plt.ylim((0, 0.3))
     for i, result in enumerate(q_results):
         axs[i].bar(*zip(*result.items()))
         axs[i].set_title(f"{plot_points[i]} steps")
+        axs[i].get_xaxis().set_ticks([])
+        axs[i].get_yaxis().set_ticks([])
 
     # Show fig
+    plt.savefig("example_quantum_vs_classical.png", dpi=600)
     plt.show()
 
 
@@ -44,10 +54,27 @@ def main():
 
     # Construct graph
     graph = nx.DiGraph()
-    graph.add_edges_from([
-        (0, 1), (0, 2), (0, 3), (0, 5), (1, 0), (1, 2), (1, 3), (2, 1), (2, 6),
-        (3, 5), (4, 1), (4, 5), (5, 2), (6, 4), (7, 1), (7, 2), (7, 6)
-        ])
+    graph.add_edges_from(
+        [
+            (0, 1),
+            (0, 2),
+            (0, 3),
+            (0, 5),
+            (1, 0),
+            (1, 2),
+            (1, 3),
+            (2, 1),
+            (2, 6),
+            (3, 5),
+            (4, 1),
+            (4, 5),
+            (5, 2),
+            (6, 4),
+            (7, 1),
+            (7, 2),
+            (7, 6),
+        ]
+    )
 
     # Create PageRank objects
     classical_pagerank = pagerank.ClassicalPageRank(graph)
